fb73a866574583e5b77701188a8a7d9a4910afbd
Mark Christiaens <mark.christiaens@sigasi.com>
2020-11-15 23:38:22 +0100

    Chapter 5

--- exercise-5.3-i/Main.hs
+++ exercise-5.3-i/Main.hs
@@ -31,18 +31,44 @@ hHead (t :# _) = t
 showBool :: HList '[_1, Bool, _2] -> String
 showBool (_ :# b :# _ :# HNil) = show b
 
-instance Eq (HList '[]) where
-    HNil == HNil = True
+-- instance Eq (HList '[]) where
+--     HNil == HNil = True
 
-instance (Eq t, Eq (HList ts)) => Eq (HList (t ': ts)) where
-    (a :# as) == (b :# bs) = a == b && as == bs
+-- instance (Eq t, Eq (HList ts)) => Eq (HList (t ': ts)) where
+--     (a :# as) == (b :# bs) = a == b && as == bs
 
 -- Note : the above cannot check HLists of different lengths
 
-instance Ord (HList '[]) where
-    compare HNil HNil = EQ
+-- instance Ord (HList '[]) where
+--     compare HNil HNil = EQ
 
-instance (Ord t, Ord (HList ts)) => Ord (HList (t ': ts)) where
+-- instance (Ord t, Ord (HList ts)) => Ord (HList (t ': ts)) where
+--     compare (a :# as) (b :# bs) = if cab == EQ then compare as bs else cab
+--         where cab = compare a b
+
+-- instance Show (HList '[]) where
+--     show HNil = "HNil"
+
+-- instance (Show t, Show (HList ts)) => Show (HList (t ': ts)) where
+--     show (t :# ts) = show t ++ ":#" ++ show ts
+
+-- show True :# HNil
+
+type family AllEq (ts :: [Type]) :: Constraint where
+    AllEq '[] = ()
+    AllEq (t ': ts) = (Eq t, AllEq ts) 
+
+type family All (c :: Type -> Constraint) (ts :: [Type]) :: Constraint where
+    All _ '[] = () 
+    All c (t ': ts) = (c t, All c ts)
+
+instance All Eq ts => Eq (HList ts) where
+    (a :# as) == (b :# bs) = a == b && as == bs    
+
+instance (All Ord ts, All Eq ts) => Ord (HList ts) where
     compare (a :# as) (b :# bs) = if cab == EQ then compare as bs else cab
         where cab = compare a b
 
+instance (All Show ts) => Show (HList ts) where
+    show HNil = "HNil"
+    show (t :# ts) = show t ++ ":#" ++ show ts
